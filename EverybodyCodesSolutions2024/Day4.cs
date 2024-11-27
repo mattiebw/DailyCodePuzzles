a66cf86e983d8b5d017ac62e9009570804473cea
Matt Ware <mattbware3@gmail.com>
2024-11-27 01:06:09 +0000

    EC Day 4 + 5

---
+++ EverybodyCodesSolutions2024/Day4.cs
@@ -0,0 +1,57 @@
+﻿using Core;
+
+namespace EverybodyCodesSolutions2024;
+
+[Solution(4)]
+public class Day4
+{
+    public static void Task1And2(string input)
+    {
+        var lengths = input.Split('\n').Select(s => Convert.ToInt32(s.Trim())).ToArray();
+
+        int smallest = lengths[0];
+        foreach (var length in lengths)
+        {
+            if (length < smallest)
+                smallest = length;
+        }
+
+        int totalDiff = 0;
+        foreach (var length in lengths) 
+            totalDiff += length - smallest;
+        
+        Console.WriteLine($@"Total hits needed: {totalDiff}");
+    }
+    
+    [Task(1)]
+    public static void Task1(string input)
+    {
+        Task1And2(input);
+    }
+    
+    [Task(2)]
+    public static void Task2(string input)
+    {
+        Task1And2(input);
+    }
+
+    [Task(3)]
+    public static void Task3(string input)
+    {
+        var lengths = input.Split('\n').Select(s => Convert.ToInt32(s.Trim())).ToList();
+
+        // long sum = 0;
+        // foreach (var length in lengths) 
+        //     sum += length;
+        // long avg = sum / lengths.Length;
+
+        lengths.Sort();
+        int med = lengths[lengths.Count / 2];
+
+        long totalDiff = 0;
+        foreach (var length in lengths) 
+            totalDiff += Math.Abs(med - length);
+        
+        Console.WriteLine($@"Total hits needed: {totalDiff}");
+    }
+}
---
+++ EverybodyCodesSolutions2024/Day5.cs
@@ -0,0 +1,191 @@
+﻿using System.Text;
+using Core;
+
+namespace EverybodyCodesSolutions2024;
+
+[Solution(5)]
+public class Day5
+{
+    [Task(1)]
+    public static void Task1(string input)
+    {
+        const int columns = 4;
+        List<int>[] seats = new List<int>[columns];
+        for (int i = 0; i < columns; i++)
+            seats[i] = new List<int>();
+        // Load initial state
+        var rows = input.Split('\n');
+        for (var rowIndex = 0; rowIndex < rows.Length; rowIndex++)
+        {
+            var row = rows[rowIndex];
+            var seatList = row.Split(' ');
+            for (var seatIndex = 0; seatIndex < columns; seatIndex++)
+            {
+                seats[seatIndex].Add(int.Parse(seatList[seatIndex].Trim()));
+            }
+        }
+        
+        // Simulate
+        const int rounds = 10;
+        string lastShoutedNumber = "";
+        for (int round = 0; round < rounds; round++)
+        {
+            // Pop off our current clapper
+            int currentCol = round % columns;
+            int num = seats[currentCol][0];
+            seats[currentCol].RemoveAt(0);
+            
+            // Add our clapper to the next column in their respective position
+            currentCol = (currentCol + 1) % columns;
+            // Find their index:
+            int periodLength = 2 * seats[currentCol].Count + 2;
+            int pos = (num - 1) % periodLength;
+            if (pos > seats[currentCol].Count)
+                pos = seats[currentCol].Count - ((pos - 1) - seats[currentCol].Count);
+            seats[currentCol].Insert(pos, num);
+            // This is apparently correct, and my brain has no idea how I got it right, but I'm happy.
+            // Next time, I need some paper to draw this out, cause my brain hurty
+
+            // Shout the number
+            StringBuilder builder = new();
+            for (int i = 0; i < columns; i++) 
+                builder.Append(seats[i][0]);
+            lastShoutedNumber = builder.ToString();
+            
+            Console.WriteLine($@"Round {round + 1}: {lastShoutedNumber}");
+        }
+        
+        Console.WriteLine($@"Last shouted number: {lastShoutedNumber}");
+    }
+    
+    [Task(2)]
+    public static void Task2(string input)
+    {
+        const int columns = 4;
+        List<int>[] seats = new List<int>[columns];
+        for (int i = 0; i < columns; i++)
+            seats[i] = new List<int>();
+        // Load initial state
+        var rows = input.Split('\n');
+        for (var rowIndex = 0; rowIndex < rows.Length; rowIndex++)
+        {
+            var row = rows[rowIndex];
+            var seatList = row.Split(' ');
+            for (var seatIndex = 0; seatIndex < columns; seatIndex++)
+            {
+                seats[seatIndex].Add(int.Parse(seatList[seatIndex].Trim()));
+            }
+        }
+        
+        // Simulate
+        Dictionary<long, int> timesShouted = new();
+        int round = 0;
+        while (true)
+        {
+            // Pop off our current clapper
+            int currentCol = round % columns;
+            int num = seats[currentCol][0];
+            seats[currentCol].RemoveAt(0);
+            
+            // Add our clapper to the next column in their respective position
+            currentCol = (currentCol + 1) % columns;
+            // Find their index:
+            // int periodLength = 2 * seats[currentCol].Count;
+            // int pos = (num - 1) % periodLength;
+            // if (pos > seats[currentCol].Count)
+            //     pos = seats[currentCol].Count - ((pos - 1) - seats[currentCol].Count);
+            
+            int pos = Math.Abs((num % (seats[currentCol].Count * 2)) - 1);
+            if (pos > seats[currentCol].Count)
+                pos = (seats[currentCol].Count * 2) - pos;
+            seats[currentCol].Insert(pos, num);
+            
+            // Shout the number
+            StringBuilder builder = new();
+            for (int i = 0; i < columns; i++) 
+                builder.Append(seats[i][0]);
+            long shoutedNumber = long.Parse(builder.ToString());
+
+            if (round < 100)
+            {
+                Console.WriteLine($@"Inserting {num} at {pos} in column {currentCol}");
+                Console.WriteLine($@"On round {round + 1}, we shouted {shoutedNumber}.");
+            }
+            
+            if (timesShouted.ContainsKey(shoutedNumber))
+            {
+                timesShouted[shoutedNumber]++;
+                if (timesShouted[shoutedNumber] == 2024)
+                {
+                    Console.WriteLine($@"On round {round + 1}, we shouted {shoutedNumber} for the 2024th time. Our answer is: {(round + 1) * shoutedNumber}");
+                    break;
+                }
+            } else timesShouted.Add(shoutedNumber, 1);
+            
+            round++;
+        }
+    }
+    
+    [Task(3)]
+    public static void Task3(string input)
+    {
+        const int columns = 4;
+        List<int>[] seats = new List<int>[columns];
+        for (int i = 0; i < columns; i++)
+            seats[i] = new List<int>();
+        // Load initial state
+        var rows = input.Split('\n');
+        for (var rowIndex = 0; rowIndex < rows.Length; rowIndex++)
+        {
+            var row = rows[rowIndex];
+            var seatList = row.Split(' ');
+            for (var seatIndex = 0; seatIndex < columns; seatIndex++)
+            {
+                seats[seatIndex].Add(int.Parse(seatList[seatIndex].Trim()));
+            }
+        }
+        
+        // Simulate
+        HashSet<string> seenStates = new();
+        int round = 0;
+        long max = 0;
+        while (true)
+        {
+            // Pop off our current clapper
+            int currentCol = round % columns;
+            int num = seats[currentCol][0];
+            seats[currentCol].RemoveAt(0);
+            
+            // Add our clapper to the next column in their respective position
+            currentCol = (currentCol + 1) % columns;
+            int pos = Math.Abs((num % (seats[currentCol].Count * 2)) - 1);
+            if (pos > seats[currentCol].Count)
+                pos = (seats[currentCol].Count * 2) - pos;
+            seats[currentCol].Insert(pos, num);
+            
+            StringBuilder stateBuilder = new();
+            for (int i = 0; i < columns; i++)
+            {
+                for (int j = 0; j < seats[i].Count; j++)
+                    stateBuilder.Append(seats[i][j]);
+                stateBuilder.Append('|');
+            }
+            string state = stateBuilder.ToString();
+            if (seenStates.Contains(state))
+            {
+                Console.WriteLine($@"We've found a previous state. Therefore, the maximum shouted number is: {max}");
+                break;
+            } 
+            seenStates.Add(state);
+            
+            // Shout the number
+            StringBuilder builder = new();
+            for (int i = 0; i < columns; i++) 
+                builder.Append(seats[i][0]);
+            long shoutedNumber = long.Parse(builder.ToString());
+            max = Math.Max(max, shoutedNumber);
+
+            round++;
+        }
+    }
+}
